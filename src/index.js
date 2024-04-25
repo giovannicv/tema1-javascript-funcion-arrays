b8740103ee90433cdaf180f1d6dfc03f56cca9a5
giovannicv <giovannivalverde@hotmail.com>
2024-04-25 13:48:17 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-//funciones
-function functionName () {
-    console.log('hi i am a function')
+//desestructuracion, de arreglos, y de objetos.
+const person = {
+    name: 'Ricardo',
+    lastName: 'Flores',
+    cars:[ 
+        'Nissan', 'Toyota'
+    ],
+    sons: ['Pepito', 'Ricardo'],
+    salary: () =>   {
+        {
+            const perDay = 100;
+            let total = 0;
+            for (let i = 0; i < 30; i++) {
+                total += perDay;
+            }
+        return total;
+        }
+    }
 }
-functionName = () => {
-    console.log('overwrite function');
-};
-//usar esta funcion
-const functionName2 = () => {
-    console.log('hi i am a function 2')
-}
-functionName();
+//arreglo
+const list = ['perro', 'gato', 'raton', 'cabra'];
+//objeto desestructuracion
+const name = 'another data';
+const { name: personName, sons, salary} = person;
+console.log(salary());
+console.log(personName, sons);
+const [,,item] = list;
+console.log(item);
